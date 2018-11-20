6d582cb3aaa906f0c1ec00969154160262afe041
Siva Kumar <sivakumar@sivakumars-macbook-air.local>
2018-11-20 20:13:56 +0530

    Initial Commit

---
+++ TableViewGET/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewGET
+//
+//  Created by sivakumar on 20/11/18.
+//  Copyright © 2018 sivakumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
